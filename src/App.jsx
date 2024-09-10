3febfe13579cac698e16cdbe12893eb675484a44
Arjun Iyer <iarjun2001@gmail.com>
2024-09-10 10:52:59 -0400

    Changed cards in skills and about sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/header/Header'
 import Navbar from './components/nav/Nav';
 import About from './components/about/About';
-import Skills from './components/Skills/Skills';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
